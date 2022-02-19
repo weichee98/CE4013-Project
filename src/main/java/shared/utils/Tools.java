02cb306bea4f19e80bea967f6ea838a45dc6e3db
lks1226 <56990594+lks1226@users.noreply.github.com>
2022-02-19 11:43:38 +0800

    Add utils

---
+++ src/main/java/shared/utils/Tools.java
@@ -0,0 +1,10 @@
+package main.java.shared.utils;
+
+public class Tools {
+    public static int findEndOfString(byte[] bytes, int start){
+        for (int i = start; i < bytes.length; i++){
+            if (bytes[i] == (byte) '\0') return i;
+        }
+        return -1;
+    }
+}
