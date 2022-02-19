8c0df76fc2ae627c5a7a1bd668bf62f4ad2fde2b
weichee98 <56989558+weichee98@users.noreply.github.com>
2022-02-19 13:08:44 +0800

    Implemented tobytes and frombytes

--- src/main/java/shared/request/CloseAccountRequest.java
+++ src/main/java/shared/request/CloseAccountRequest.java
@@ -1,15 +1,56 @@
 package main.java.shared.request;
 
+import main.java.shared.utils.Tools;
+
+import java.nio.ByteBuffer;
+import java.util.Arrays;
+
 public class CloseAccountRequest extends RequestBody {
     private final int accountNumber;
     private final String holderName;
     private final String password;
 
 
-    public CloseAccountRequest(String holderName, String password, int accountNumber) {
+    public CloseAccountRequest(int accountNumber, String holderName, String password) {
+        this.accountNumber = accountNumber;
         this.holderName = holderName;
         this.password = password;
-        this.accountNumber = accountNumber;
+    }
+
+    public static CloseAccountRequest fromBytes(byte[] bytes) {
+        int accountNumber = ByteBuffer.wrap(
+                Arrays.copyOfRange(bytes, 0, 4)
+        ).getInt();
+
+        int ptr = 4;
+        int holderNameEnd = Tools.findEndOfString(bytes, ptr);
+        String holderName = new String(
+                Arrays.copyOfRange(bytes, ptr, holderNameEnd)
+        );
+        ptr = holderNameEnd + 1;
+
+        int passwordEnd = Tools.findEndOfString(bytes, ptr);
+        String password = new String(
+                Arrays.copyOfRange(bytes, ptr, passwordEnd)
+        );
+        ptr = passwordEnd + 1;
+
+        return new CloseAccountRequest(
+                accountNumber,
+                holderName,
+                password
+        );
+    }
+
+    public static void main(String[] args) {
+        CloseAccountRequest c = new CloseAccountRequest(
+                8014374,
+                "lsad",
+                "asdliadasa"
+        );
+        byte[] bytes = c.toBytes();
+        System.out.println(Arrays.toString(bytes));
+        System.out.println(CloseAccountRequest.fromBytes(bytes));
     }
 
     public int getAccountNumber() {
@@ -26,9 +67,39 @@ public class CloseAccountRequest extends RequestBody {
 
     @Override
     public byte[] toBytes() {
-//        TODO - toBytes
-        return new byte[0];
+        byte[] accountNumberByte = ByteBuffer.allocate(4)
+                .putInt(this.accountNumber).array();
+        byte[] holderNameByte = (this.holderName + "\0").getBytes();
+        byte[] passwordByte = (this.password + "\0").getBytes();
+
+        final int totalBytes = accountNumberByte.length +
+                holderNameByte.length + passwordByte.length;
+        byte[] bytes = new byte[totalBytes];
+
+        System.arraycopy(
+                accountNumberByte, 0, bytes, 0, accountNumberByte.length
+        );
+        int ptr = accountNumberByte.length;
+
+        System.arraycopy(
+                holderNameByte, 0, bytes, ptr, holderNameByte.length
+        );
+        ptr += holderNameByte.length;
+
+        System.arraycopy(
+                passwordByte, 0, bytes, ptr, passwordByte.length
+        );
+        ptr += passwordByte.length;
+
+        return bytes;
     }
 
-
+    @Override
+    public String toString() {
+        return "CloseAccountRequest{" +
+                "accountNumber=" + accountNumber +
+                ", holderName='" + holderName + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
 }
--- src/main/java/shared/request/OpenAccountRequest.java
+++ src/main/java/shared/request/OpenAccountRequest.java
@@ -1,6 +1,10 @@
 package main.java.shared.request;
 
 import main.java.shared.entity.Currency;
+import main.java.shared.utils.Tools;
+
+import java.nio.ByteBuffer;
+import java.util.Arrays;
 
 public class OpenAccountRequest extends RequestBody {
     private final String holderName;
@@ -13,6 +17,48 @@ public class OpenAccountRequest extends RequestBody {
         this.password = password;
         this.currency = currency;
         this.balance = balance;
+    }
+
+    public static OpenAccountRequest fromBytes(byte[] bytes) {
+        int ptr = 0;
+
+        int holderNameEnd = Tools.findEndOfString(bytes, ptr);
+        String holderName = new String(
+                Arrays.copyOfRange(bytes, ptr, holderNameEnd)
+        );
+        ptr = holderNameEnd + 1;
+
+        int passwordEnd = Tools.findEndOfString(bytes, ptr);
+        String password = new String(
+                Arrays.copyOfRange(bytes, ptr, passwordEnd)
+        );
+        ptr = passwordEnd + 1;
+
+        Currency currency = Currency.fromByte(bytes[ptr]);
+        ptr += 1;
+
+        float balance = ByteBuffer.wrap(
+                Arrays.copyOfRange(bytes, ptr, ptr + 4)
+        ).getFloat();
+
+        return new OpenAccountRequest(
+                holderName,
+                password,
+                currency,
+                balance
+        );
+    }
+
+    public static void main(String[] args) {
+        OpenAccountRequest o = new OpenAccountRequest(
+                "abc",
+                "asdagasgsk",
+                Currency.USD,
+                (float) 123.45678
+        );
+        byte[] bytes = o.toBytes();
+        System.out.println(Arrays.toString(bytes));
+        System.out.println(OpenAccountRequest.fromBytes(bytes));
     }
 
     public String getHolderName() {
@@ -33,8 +79,33 @@ public class OpenAccountRequest extends RequestBody {
 
     @Override
     public byte[] toBytes() {
-//        TODO - toBytes
-        return new byte[0];
+        byte[] holderNameByte = (this.holderName + "\0").getBytes();
+        byte[] passwordByte = (this.password + "\0").getBytes();
+        byte currencyByte = this.currency.toByte();
+        byte[] balanceByte = ByteBuffer.allocate(4).putFloat(this.balance).array();
+
+        final int totalBytes = 1 + holderNameByte.length +
+                passwordByte.length + balanceByte.length;
+        byte[] bytes = new byte[totalBytes];
+
+        int ptr = 0;
+        System.arraycopy(
+                holderNameByte, 0, bytes, ptr, holderNameByte.length
+        );
+        ptr += holderNameByte.length;
+
+        System.arraycopy(
+                passwordByte, 0, bytes, ptr, passwordByte.length
+        );
+        ptr += passwordByte.length;
+
+        bytes[ptr] = currencyByte;
+        ptr += 1;
+
+        System.arraycopy(
+                balanceByte, 0, bytes, ptr, balanceByte.length
+        );
+        return bytes;
     }
 
     @Override
